ba448af808975b124719de398f31d0fe834323cc
mahdieh1992 <mohamadimahdieh70@gmail.com>
2024-07-04 15:54:59 +0330

    responsive header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,7 +1,7 @@
 
 export default function Header() {
     return (
-        <header className="w-[90%] bg-black/50 mx-auto flex justify-between items-center rounded-3xl text-xl py-5 px-10 mt-9 ">
+        <header className=" w-[90%] bg-black/50 mx-auto  hidden md:flex justify-between items-center rounded-3xl text-xl py-5 px-3.5 lg:px-10 mt-9 ">
             <svg className="hidden">
                 <symbol id="shopping-cart" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
@@ -16,13 +16,13 @@ export default function Header() {
 
             </svg>
            {/* Menu & icon */}
-           <nav className="flex gap-x-9">
+           <nav className="flex gap-x-3 lg:gap-x-9 items-center">
             {/* Logo icon */}
              <div>
                 <img src="../images/app-logo.png" alt="logo-png"></img>
              </div>
               {/* Menu */}
-             <ul className="flex gap-x-9  text-gray-300 font-DanaRegular tracking-tightest">
+             <ul className="flex  gap-x-3 lg:gap-x-9  text-gray-300 font-DanaRegular tracking-tightest">
                 <li>صفحه اصلی</li>
                 <li>فروشگاه</li>
                 <li>دیکشنری</li>
@@ -32,9 +32,9 @@ export default function Header() {
              </ul>
            </nav>
            {/* icon & login */}
-           <div className="flex items-center gap-x-5 text-orange-200">
+           <div className="flex items-center gap-x-2.5 lg:gap-x-5 text-orange-200">
             {/* icons */}
-                <div className="flex gap-x-5">
+                <div className="flex  gap-x-2.5 lg:gap-x-5">
                     {/* shopping-cart */}
                     <svg className="w-[36px] h-[36px]">
                         <use href="#shopping-cart"></use>
